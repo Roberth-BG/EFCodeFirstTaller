27c81b2bc9c380966acfcca6707a131fbe51ee6a
Roberth-BG <roberthbg2026@hotmail.com>
2026-09-21 23:37:12 -0500

    agregado migracion a la base de datos

--- EFCodeFirstTaller/Models/Producto.cs
+++ EFCodeFirstTaller/Models/Producto.cs
@@ -1,9 +1,14 @@
-﻿namespace EFCodeFirstTaller.Models;
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace EFCodeFirstTaller.Models;
 
 public class Producto
 {
     public int Id { get; set; }
     public string Nombre { get; set; } = string.Empty;
+
+    [Column(TypeName = "decimal(18,2)")]
     public decimal Precio { get; set; }
+    
     public int Stock { get; set; }
 }
