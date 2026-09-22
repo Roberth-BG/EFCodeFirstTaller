3c41e0a3b27087d1d092f2dd6181b25467edf35b
Roberth-BG <roberthbg2026@hotmail.com>
2026-09-21 21:03:43 -0500

    Inicio del proyecto

---
+++ EFCodeFirstTaller/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace EFCodeFirstTaller.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
