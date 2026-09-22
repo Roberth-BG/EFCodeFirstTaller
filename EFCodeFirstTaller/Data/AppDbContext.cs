f81463c68d019cbaa899a702e8793800120471f0
Roberth-BG <roberthbg2026@hotmail.com>
2026-09-21 21:59:40 -0500

    agregado AppDbContext mostrando el DbSet Producto

---
+++ EFCodeFirstTaller/Data/AppDbContext.cs
@@ -0,0 +1,14 @@
+﻿using EFCodeFirstTaller.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace EFCodeFirstTaller.Data;
+
+public class AppDbContext : DbContext
+{
+    public AppDbContext(DbContextOptions<AppDbContext> options)
+        : base(options)
+    {
+    }
+
+    public DbSet<Producto> Productos { get; set; } = null!;
+}
